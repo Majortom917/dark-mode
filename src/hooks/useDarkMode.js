e41029f58b458d7b83ba94e3c2e4b8ea35a43a2d
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-28 14:01:14 -0700

    navbar set to remember darkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,3 +1,22 @@
 import useLocalStorage from './useLocalStorage'
+import {useEffect} from 'react';
 
-const [someValue, setSomeValue] = useLocalStorage('your key here')
+
+const useDarkMode = (initialValue) => {
+
+    const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.querySelector('body').classList.add('dark-mode');
+        } else {
+            document.querySelector('body').classList.remove('dark-mode');
+        }
+    }, [darkMode]);
+
+  return (
+      [ darkMode, setDarkMode ]
+  );
+};
+
+export default useDarkMode;
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,10 +5,12 @@ export const useLocalStorage = (key, initialValue) => {
       const item = localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     });
-  
-    return [storedValue];
-  };
+
   const setValue = value => {
     setStoredValue(value);
     localStorage.setItem(key, JSON.stringify(value));
-  };
+  }; 
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage
